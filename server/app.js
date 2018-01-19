766d16b4609b00d0bbbf6855e49c0aeb1751fad9
nickvhite <nickvhite@gmail.com>
2018-01-19 13:10:58 +0200

    add client

--- server/app.js
+++ server/app.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('*', function(req, res, next) {
+    if(req.session.user){
+        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    }
+});
 
 module.exports = app;
